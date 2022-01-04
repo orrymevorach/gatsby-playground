bd8499317e66e071334f21eb68c4eac686e71e8f
Orry Mevorach <orry.mevorach@gmail.com>
2022-01-03 19:48:55 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,21 @@
+require('dotenv').config({
+  path: `.env`,
+});
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Playground",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Gatsby Playground',
   },
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: "oiylgdxnvK6fsZWbMkQWjxBQgIRRGzhsssXoHjtC4ms",
-        spaceId: "",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
   ],
 };
